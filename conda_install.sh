914633461613d5e6f9ba27ce2392926d34832018
mamerl <maximilian.amerl@cern.ch>
2025-11-10 11:45:16 +0000

    Modify setup and install scripts

--- conda_install.sh
+++ conda_install.sh
@@ -18,9 +18,13 @@ conda env create -f conda_environment.yml -y
 conda activate dmwg-coupling-scan 
 
 # LHAPDF setup 
-# install the pdf sets
+# install the pdf sets (these will go into the conda environment)
+# NOTE: you can add more pdf sets here if needed
 lhapdf install NNPDF30_nlo_as_0118
 lhapdf install NNPDF30_nlo_as_0118_hessian
+
+# install the DMWG-couplingScan-code package
+cd DMWG-couplingScan-code
 
 # install the pybind11 code in src
 pip install -e .
--- conda_setup.sh
+++ conda_setup.sh
@@ -1,9 +1,10 @@
 conda deactivate 
 conda activate dmwg-coupling-scan
 
+# add this directory to the PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
 # source the setup script
-TOP_LEVEL_DIR=$(cd .. && pwd)
-DMWG_COUPLING_SCAN_DIR=$TOP_LEVEL_DIR/DMWG-couplingScan-code
-
-source $DMWG_COUPLING_SCAN_DIR/setup.sh
-
+DMWG_COUPLING_SCAN_DIR=$(pwd)/DMWG-couplingScan-code
+cd $DMWG_COUPLING_SCAN_DIR
+source setup.sh # this will setup additional LHAPDF environment variables
